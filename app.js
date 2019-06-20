51d19b4b6f522aa01f92841ff2b3869973277b6b
Zarnerz <holstein30@live.com>
2019-06-20 19:49:50 -0400

    laid out some ground rules for modular design in JS file

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// App
+// App - Using Modular Design
+
+// * Ground Rules
+// Self contained module
+// - everything to do with my module is in my module
+// - no global variables
+// - if a module manages more than one thing it should be split up
+// Separation of concerns
+// DRY code
+// efficient DOM usage
+// - very few $(selections)
+// no memory leaks
+// - all events can be unbound
