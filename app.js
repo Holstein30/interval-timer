b1f8543aee6d410290bb20af01916ae825bc9ad1
Dale Holstein <holstein30@live.com>
2019-06-22 10:57:06 -0400

    timer set and counting down in console

--- app.js
+++ app.js
@@ -11,3 +11,27 @@
 // - very few $(selections)
 // no memory leaks
 // - all events can be unbound
+
+let countdown;
+// const timerDisplay = document.querySelector(".display__time-left");
+// const endTime = document.querySelector(".display__end-time");
+// const buttons = document.querySelectorAll("[data-time]");
+
+function timer(seconds) {
+  // clear any exisiting timers
+  clearInterval(countdown);
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  //   displayTimeLeft(seconds);
+  //   displayEndTime(then);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check if we should stop it!
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    console.log(secondsLeft);
+  }, 1000);
+}
